981e75a2d2e4f1c04f9295bb5bb1a5778e53181a
Jeremy Ruroede <jeremyruroede@gmail.com>
2021-03-18 22:02:58 -0400

    Update minestartup.py

--- minestartup.py
+++ minestartup.py
@@ -2,6 +2,6 @@
 import os
 
 def auto():
-    os.system('./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u YOUR_EMAIL') 
+    os.system('/opt/cpuminer-multi/cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u YOUR_EMAIL') 
      
 auto()
